2c2ef8c59b3af3117436d70081ffefacf30c19d4
khatri-jatin <jatinkhatri340@gmail.com>
2025-07-10 14:55:24 +0530

    index.js updated ag

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, Jenkins!');
+  res.send('Hello, Jenkins!');
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+if (require.main === module) {
+  // Only listen if this file is run directly (not required/imported)
+  app.listen(3000, () => console.log('Server running on port 3000'));
+}
+
+module.exports = app;
 
